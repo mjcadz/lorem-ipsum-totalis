e22aaa10b8bcb0409fb237ee46f10838abe3a7fb
mjcadz <mjcadzdev@gmail.com>
2022-07-16 18:47:34 +0930

    :seedling: feat(ui): add spinner component

--- shared/ui/src/index.ts
+++ shared/ui/src/index.ts
@@ -1,3 +1,4 @@
 export { default as uiButton } from '@/components/Button.vue'
 export { default as uiInput } from '@/components/Input.vue'
 export { default as uiListbox } from '@/components/Listbox.vue'
+export { default as uiSpinner } from '@/components/Spinner.vue'
